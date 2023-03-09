171dafe2a16a6047a8b25ce713a55f8b40a735f2
RayaneDumont <rayane.dumont@gmail.com>
2023-03-09 13:48:05 +0100

    add new bouton to personnal space for playing

--- src/components/Personal_space/index.js
+++ src/components/Personal_space/index.js
@@ -20,7 +20,7 @@ function PersonalSpace() {
   let scoreUser = useSelector((state) => state.login.score);
 
   //All users
-  const ranking = useSelector((state) => state.ranking.classement[0]);
+  const ranking = useSelector((state) => state.ranking.classement);
 
   if (ranking !== undefined) {
     // we find the user connected in the ranking
@@ -43,21 +43,32 @@ function PersonalSpace() {
 
 if (logged) {
   return (
-    <div className="stats">      
-      <Button 
-        className="Button"
-        variant="success"
-        size="lg"
-        >
-        <NavLink to="/compte/ajout-film" className="stats__link-add">
-          <span>Ajouter un film</span>
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none">
-            <style>{`@keyframes slide-right{0%{transform:translateY(0)}to{transform:translateY(1px)}}`}</style>
-            <path fill="#265BFF" d="M12.75 6.432a.75.75 0 00-1.5 0h1.5zm-1.5 6a.75.75 0 001.5 0h-1.5zm-1.22-2.53a.75.75 0 10-1.06 1.06l1.06-1.06zm1.97 3.03l-.53.53a.75.75 0 001.06 0l-.53-.53zm3.03-1.97a.75.75 0 00-1.06-1.06l1.06 1.06zm-3.78-4.53v6h1.5v-6h-1.5zm-2.28 4.53l2.5 2.5 1.06-1.06-2.5-2.5-1.06 1.06zm3.56 2.5l2.5-2.5-1.06-1.06-2.5 2.5 1.06 1.06z" style={{animation: "slide-right .5s cubic-bezier(1,-.43,.68,.57) infinite alternate both"}} />
-            <path stroke="#0A0A30" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M8 17.274h8" />
-          </svg>
-        </NavLink>
-      </Button>
+    <div className="stats">
+      <div className="stats--btn">
+        <Button 
+          className="Button"
+          variant="success"
+          size="lg"
+          >
+          <NavLink to="/jeu" className="stats__link-play">
+            <span>Jouer</span>
+          </NavLink>
+        </Button>
+        <Button 
+          className="Button"
+          variant="success"
+          size="lg"
+          >
+          <NavLink to="/compte/ajout-film" className="stats__link-add">
+            <span>Ajouter un film</span>
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none">
+              <style>{`@keyframes slide-right{0%{transform:translateY(0)}to{transform:translateY(1px)}}`}</style>
+              <path fill="#265BFF" d="M12.75 6.432a.75.75 0 00-1.5 0h1.5zm-1.5 6a.75.75 0 001.5 0h-1.5zm-1.22-2.53a.75.75 0 10-1.06 1.06l1.06-1.06zm1.97 3.03l-.53.53a.75.75 0 001.06 0l-.53-.53zm3.03-1.97a.75.75 0 00-1.06-1.06l1.06 1.06zm-3.78-4.53v6h1.5v-6h-1.5zm-2.28 4.53l2.5 2.5 1.06-1.06-2.5-2.5-1.06 1.06zm3.56 2.5l2.5-2.5-1.06-1.06-2.5 2.5 1.06 1.06z" style={{animation: "slide-right .5s cubic-bezier(1,-.43,.68,.57) infinite alternate both"}} />
+              <path stroke="#0A0A30" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M8 17.274h8" />
+            </svg>
+          </NavLink>
+        </Button>
+      </div>
       <div className="title">
         <h1 className="title--stats">
           Vos statistiques :
